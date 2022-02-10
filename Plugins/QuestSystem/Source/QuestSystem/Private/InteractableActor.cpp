024ef575e9677ed049572ebb7e41221b900c133c
KrisKaroll <g.mishell@gmail.com>
2022-02-10 21:45:01 +0700

    plugin for quest system created. added iteratableobject interface

---
+++ Plugins/QuestSystem/Source/QuestSystem/Private/InteractableActor.cpp
@@ -0,0 +1,11 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "InteractableActor.h"
+
+
+void AInteractableActor::OnInteractionFinished(AActor * ActorInteractedWithObject)
+{
+	NotifyInteractionFinished(this, ActorInteractedWithObject);
+}
+
